a5df528a97fadb68d8f65be9184cb2ddd829dfff
pppp111164730 <longleg206@gmail.com>
2023-02-28 09:58:47 -0600

    finds loop with the values at the node can me modified easily for nodes

--- cyclic.py
+++ cyclic.py
@@ -1,14 +1,71 @@
 from llist import *
 
+def find(lst, num):
+    if lst.head:
+        nex = lst.head
+        while nex.val != num and nex.next != None:
+            nex = nex.next
+        if nex.val == num:
+            return nex
+
+def findtail(lst):
+    if lst.head:
+        nex = lst.head
+        while nex.next:
+            nex = nex.next
+        lst.tail = nex
+
 def llprint(lst, num):
-    """print the first num terms of linked list lst"""
-    pass
+    if lst.head:
+        nex = lst.head
+        i = 0
+        while i < num and nex.next:
+            print(nex.val)
+            i += 1
+            nex = nex.next
 
 def find_cycle(lst):
-    """return the start index and length of the cycle"""
-    pass
+    if lst.head:
+        nex = lst.head
+        if lst.head.next:
+            nev = lst.head.next
+            while nex != nev and nev.next: 
+                nev = nev.next
+                if nev == nex:
+                    csl = []
+                    csl.append(nex.val)
+                    nec = nex.next
+                    while nec != nex:
+                        csl.append(nec.val)
+                        nec = nec.next
+                    return csl
+                nev = nev.next
+                if nev == nex:
+                    csl = []
+                    csl.append(nex.val)
+                    nec = nex.next
+                    while nec != nex:
+                        csl.append(nec.val)
+                        nec = nec.next
+                    return csl
+                nex = nex.next
+        else:
+            return False
+    else:
+        return False
+        
+
 
 
 if __name__ == "__main__":
 
-    pass
+    lst = LList()
+    for i in range(0,15):
+        append(lst, Node(2**i))
+    findtail(lst)
+    lst.tail.next = find(lst, 1024)
+
+    
+    print(find_cycle(lst))
+
+
